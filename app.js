75f9b6863ef0b6c8a8a1d4feff4d15c6964f76f1
Lewis Ndegwa <lewiskanyi77@gmail.com>
2023-09-03 18:07:06 +0300

    responsive slideshow

--- app.js
+++ app.js
@@ -21,4 +21,36 @@ const navSlide = () => {
     });
 }
 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type:"fractiion",
+    },
+    autoplay:{
+        delay:3000,
+        disableOnInteraction:false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    effect:"creative",
+    creativeEffect: {
+        prev:{
+            shadow:true,
+            translate: [0,0-400],
+        },
+        next:{
+            translate: ["100%",0,0]
+        },
+    },
+  });
+
+
+
 navSlide();
