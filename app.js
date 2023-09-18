2f60636a5cc973362be617a4e5ef460982a016c8
Lewis Ndegwa <lewiskanyi77@gmail.com>
2023-09-18 11:54:31 +0300

    scroll to top button

--- app.js
+++ app.js
@@ -110,3 +110,35 @@ document.querySelectorAll('a[href^="#id_intro_container"]').forEach(link => {
   });
 
 navSlide();
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  let mybutton = document.getElementById("myBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+  } else {
+      mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document smoothly
+function topFunction() {
+  if (window.scrollTo) { // Check if the browser supports smooth scrolling
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth' // Add smooth behavior
+      });
+  } else { // For browsers that don't support smooth scrolling, scroll instantly
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  }
+}
