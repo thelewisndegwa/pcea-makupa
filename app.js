6bdabede2c43e342207cdd7c3bd53265f16f2e48
Lewis Ndegwa <lewiskanyi77@gmail.com>
2023-09-23 16:52:00 +0300

    nav bar fixed

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
- 
-  
-    burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');  
-        
-        // Animate links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation){
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }            
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    });
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+      // Toggle Nav
+      nav.classList.toggle('nav-active');
+      
+      // Toggle body scroll (optional, to prevent scrolling when the menu is open)
+      document.body.classList.toggle('no-scroll');
+
+      // Animate links
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = '';
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+          }
+      });
+
+      // Burger Animation
+      burger.classList.toggle('toggle');
+  });
 }
+
+// Call the navSlide function to initialize the navigation menu behavior
+navSlide();
+
+
+
 
 
 var swiper = new Swiper(".mySwiper", {
@@ -123,7 +132,7 @@ document.querySelectorAll('a[href^="#id_intro_container"]').forEach(link => {
     });
   });
 
-navSlide();
+
 
 
 // Get the button:
--- app1.js
+++ app1.js
@@ -1,26 +1,31 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
- 
-  
-    burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');  
-        
-        // Animate links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation){
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }            
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    });
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+      // Toggle Nav
+      nav.classList.toggle('nav-active');
+      
+      // Toggle body scroll (optional, to prevent scrolling when the menu is open)
+      document.body.classList.toggle('no-scroll');
+
+      // Animate links
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = '';
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+          }
+      });
+
+      // Burger Animation
+      burger.classList.toggle('toggle');
+  });
 }
-navSlide()
+
+// Call the navSlide function to initialize the navigation menu behavior
+navSlide();
 
 // Get the button:
 let mybutton = document.getElementById("myBtn");
